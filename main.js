da8d2af664610c6bec362acdd2712617ce79efc3
federicoalagna <federicoalagna01@gmail.com>
2024-04-23 15:09:48 +0200

    first commit

---
+++ main.js
@@ -0,0 +1,5 @@
+let currentElement = document.getElementById(`username`)
+const userName = prompt(`inserisci il tuo nome`);
+const userLastName = prompt(`inserisci il tuo cognome`);
+
+currentText = `${currentText} ${userName} ${userLastName}` ;

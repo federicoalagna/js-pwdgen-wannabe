6fd376e34587bed6e99c1e4d7e2e180a1d2f0e92
federicoalagna <federicoalagna01@gmail.com>
2024-04-23 16:09:49 +0200

    input completo

--- main.js
+++ main.js
@@ -1,5 +1,11 @@
-let currentElement = document.getElementById(`username`)
+'use strict'
+
+let currentElement = document.getElementById(`username`);
+let currentText = currentElement.innerHTML;
 const userName = prompt(`inserisci il tuo nome`);
 const userLastName = prompt(`inserisci il tuo cognome`);
+const userColor = prompt(`inserisci il tuo colore preferito`);
 
-currentText = `${currentText} ${userName} ${userLastName}` ;
+currentText = `${currentText}${userName}${userLastName}${userColor}01` ;
+console.log(currentText);
+currentElement.innerHTML = currentText;
